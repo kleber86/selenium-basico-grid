f00523c317f35d81533c0ffe93df310622b2734a
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-03-18 10:38:48 -0300

    trabalhando com o combo

--- src/test/java/TesteCampoTreinamento.java
+++ src/test/java/TesteCampoTreinamento.java
@@ -3,7 +3,10 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCampoTreinamento {
 
@@ -39,7 +42,7 @@ public class TesteCampoTreinamento {
     }
 
     @Test
-    public void deveInteragirComCheckBox(){
+    public void deveInteragirComCheckbox(){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("C:\\QA\\treinamento\\componentes.html");
@@ -49,5 +52,21 @@ public class TesteCampoTreinamento {
 
         Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:1")).isSelected());
 
+    }
+
+    @Test
+    public void deveInteragirComCombo(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1500,800));
+        driver.get("file:///c:/QA/treinamento/componentes.html");
+
+        WebElement elemento = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select combo = new Select(elemento);
+        //combo.selectByIndex(3);
+        //combo.selectByValue("especializacao");
+        combo.selectByVisibleText("2o grau completo");
+
+
+        Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
     }
 }
