801c38111112b155f0fd0d14d5fc446a941e037d
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-03-18 01:29:53 -0300

    setando o tamanho do browser e finalizando o navegador ao final da execução

--- src/test/java/TestaGoogle.java
+++ src/test/java/TestaGoogle.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,6 +10,7 @@ public class TestaGoogle {
     @Test
     public void testeFirefox(){
         WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
         driver.get("https://www.google.com.br");
         Assert.assertEquals("Google", driver.getTitle());
         driver.quit();
@@ -17,6 +19,7 @@ public class TestaGoogle {
     @Test
     public void testeChrome(){
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.google.com.br");
         Assert.assertEquals("Google", driver.getTitle());
         driver.quit();
