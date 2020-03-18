579aa77e31ab641c74a37f0a6dd248049d5590f4
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-03-18 01:48:05 -0300

    trabalhando com textfield e textarea

--- src/test/java/TesteCampoTreinamento.java
+++ src/test/java/TesteCampoTreinamento.java
@@ -1,4 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,7 +13,18 @@ public class TesteCampoTreinamento {
         driver.manage().window().setSize(new Dimension(1280, 800));
         driver.get("C:\\QA\\treinamento\\componentes.html");
 
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Kleber");
+        Assert.assertEquals("Kleber", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 
         driver.quit();
+    }
+
+    @Test
+    public void deveInteragirComTextArea(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1280, 800));
+        driver.get("C:\\QA\\treinamento\\componentes.html");
+
+        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Aprendendo Automação\ncom Java!\n");
     }
 }
