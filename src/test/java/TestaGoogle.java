4b74632a6588968c524e2bfec51dab1d8fd39e8c
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-03-17 22:47:44 -0300

    utilizando a automação com o chrome

--- src/test/java/TestaGoogle.java
+++ src/test/java/TestaGoogle.java
@@ -1,14 +1,24 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestaGoogle {
 
     @Test
-    public void teste(){
+    public void testeFirefox(){
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com.br");
         Assert.assertEquals("Google", driver.getTitle());
+        driver.quit();
+    }
+
+    @Test
+    public void testeChrome(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.br");
+        Assert.assertEquals("Google", driver.getTitle());
+        driver.quit();
     }
 }
